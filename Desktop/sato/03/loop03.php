9a15ca01aff0d51235212a840ca1c17718e27aa3
SatoIshii <ishii-sato@webrage.jp>
2021-08-05 15:50:16 +0900

    [add] loo1~3ファイル追加
    表出力ファイルloop1~3を追加いたしました。
    loop01:行指定表出力
    loop02:行列数指定表出力
    loop03:セル名追加

--- Desktop/sato/03/loop03.php
+++ Desktop/sato/03/loop03.php
@@ -8,9 +8,9 @@
 
   <body>
       <h1>表出力</h1>
-      <form method='post' action='loop01.php'>
-          <p>数字を入力してください。</p>
-          <input type="text" name="num" size="3">
+      <form method='post' action='loop02.php'>
+          <input type="text" name="li" size="3">行 x
+          <input type="text" name="col" size="3">列
           <br><br>
           <input type="submit" value="送信">
           <input type="reset" value="リセット">
@@ -18,8 +18,12 @@
       <br><br>
       <table border="1" style="border-collapse:collapse;">
           <?php
-          for($i=0;$i<$_POST['num'];$i++){
-            echo "<tr><td>ishii</td></tr>";
+          for($i=1;$i<=$_POST['li'];$i++){
+              echo "<tr>";
+             for($j=1;$j<=$_POST['col'];$j++){
+                 echo  "<td>" .$i . "-" . $j . "</td>";
+             }
+              echo "</tr>";
             }
           ?>
       </table>
