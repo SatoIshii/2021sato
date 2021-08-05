f863523982ac43379860d31955c347a5802fef0c
SatoIshii <ishii-sato@webrage.jp>
2021-08-05 14:23:18 +0900

    [update] 出力のアップデート
    表の出力形式を改善いたしました。

--- Desktop/sato/02/tax.php
+++ Desktop/sato/02/tax.php
@@ -62,53 +62,45 @@
       </form>
       <br>
       <?php
+            //1行目
             $name01 = $_POST["namae01"];
             $price01 = $_POST["nedan01"];
             $number01 = $_POST["num01"];
-            if(isset($_POST['tax01'])) {
-                $tax01 = $_POST["tax01"];
-                $tax = $tax01*0.01;
-            }
+            $tax01 = $_POST["tax01"];
+            $tax = $tax01*0.01;
             $sum01 = ($price01 + ($price01 * $tax)) ;
-
+            //2行目
             $name02 = $_POST["namae02"];
             $price02 = $_POST["nedan02"];
             $number02 = $_POST["num02"];
-            if(isset($_POST['tax02'])) {
-                $tax02 = $_POST["tax02"];
-                $tax = $tax02*0.01;
-            }
+            $tax02 = $_POST["tax02"];
+            $tax = $tax02*0.01;
             $sum02 = ($price02 + ($price02 * $tax)) ;
-
+            //3行目
             $name03 = $_POST["namae03"];
             $price03 = $_POST["nedan03"];
             $number03 = $_POST["num03"];
-            if(isset($_POST['tax03'])) {
-                $tax03 = $_POST["tax03"];
-                $tax = $tax03*0.01;
-            }
+            $tax03 = $_POST["tax03"];
+            $tax = $tax03*0.01;
             $sum03 = ($price03 + ($price03 * $tax)) ;
-
+            //4行目
             $name04 = $_POST["namae04"];
             $price04 = $_POST["nedan04"];
             $number04 = $_POST["num04"];
-            if(isset($_POST['tax04'])) {
-                $tax04 = $_POST["tax04"];
-                $tax = $tax04*0.01;
-            }
+            $tax04 = $_POST["tax04"];
+            $tax = $tax04*0.01;
             $sum04 = ($price04 + ($price04 * $tax)) ;
-
+            //5行目
             $name05 = $_POST["namae05"];
             $price05 = $_POST["nedan05"];
             $number05 = $_POST["num05"];
-            if(isset($_POST['tax05'])) {
-                $tax05 = $_POST["tax05"];
-                $tax = $tax05*0.01;
-            }
+            $tax05 = $_POST["tax05"];
+            $tax = $tax05*0.01;
             $sum05 = ($price05 + ($price05 * $tax)) ;
-
+            //合計計算
             $sum = $sum01 + $sum02 + $sum03 + $sum04 + $sum05;
       ?>
+
       <table  border="1" style="border-collapse:collapse;">
           <tr>
               <th>商品名</th>
@@ -118,43 +110,43 @@
               <th>小計（単位：円）</th>
           </tr>
           <tr>
-              <td><?php echo $name01;?></td>
+              <td style="text-align: left;"><?php echo $name01;?></td>
               <td style="text-align: right;"><?php echo number_format($price01);?></td>
-              <td><?php echo $number01;?></td>
-              <td><?php echo $tax01;?>%</td>
-              <td><?php echo number_format($sum01);?></td>
+              <td style="text-align: right;"><?php echo $number01;?></td>
+              <td style="text-align: right;"><?php echo $tax01;?>%</td>
+              <td style="text-align: right;"><?php echo number_format($sum01);?></td>
           </tr>
           <tr>
-              <td><?php echo $name02;?></td>
+              <td style="text-align: left;"><?php echo $name02;?></td>
               <td style="text-align: right;"><?php echo number_format($price02);?></td>
-              <td><?php echo $number02;?></td>
-              <td><?php echo $tax02;?>%</td>
-              <td><?php echo number_format($sum02);?></td>
+              <td style="text-align: right;"><?php echo $number02;?></td>
+              <td style="text-align: right;"><?php echo $tax02;?>%</td>
+              <td style="text-align: right;"><?php echo number_format($sum02);?></td>
           </tr>
           <tr>
-              <td><?php echo $name03;?></td>
+              <td style="text-align: left;"><?php echo $name03;?></td>
               <td style="text-align: right;"><?php echo number_format($price03);?></td>
-              <td><?php echo $number03;?></td>
-              <td><?php echo $tax03;?>%</td>
-              <td><?php echo number_format($sum03)?></td>
+              <td style="text-align: right;"><?php echo $number03;?></td>
+              <td style="text-align: right;"><?php echo $tax03;?>%</td>
+              <td style="text-align: right;"><?php echo number_format($sum03)?></td>
           </tr>
           <tr>
-              <td><?php echo $name04;?></td>
+              <td style="text-align: left;"><?php echo $name04;?></td>
               <td style="text-align: right;"><?php echo number_format($price04);?></td>
-              <td><?php echo $number04;?></td>
-              <td><?php echo $tax04;?>%</td>
-              <td><?php echo number_format($sum04);?></td>
+              <td style="text-align: right;"><?php echo $number04;?></td>
+              <td style="text-align: right;"><?php echo $tax04;?>%</td>
+              <td style="text-align: right;"><?php echo number_format($sum04);?></td>
           </tr>
           <tr>
-              <td><?php echo $name05;?></td>
+              <td style="text-align: left;"><?php echo $name05;?></td>
               <td style="text-align: right;"><?php echo number_format($price05);?></td>
-              <td><?php echo $number05;?></td>
-              <td><?php echo $tax05;?>%</td>
-              <td><?php echo number_format($sum05);?></td>
+              <td style="text-align: right;"><?php echo $number05;?></td>
+              <td style="text-align: right;"><?php echo $tax05;?>%</td>
+              <td style="text-align: right;"><?php echo number_format($sum05);?></td>
           </tr>
           <tr>
               <td colspan="4">合計</td>
-              <td><?php echo number_format($sum);?></td>
+              <td style="text-align: right;"><?php echo number_format($sum);?></td>
           </tr>
       </table>
   </body>
