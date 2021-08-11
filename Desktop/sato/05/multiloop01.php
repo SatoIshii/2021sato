fb54470b5697a91d9e99b7f8f0147fbd68eb0773
SatoIshii <ishii-sato@webrage.jp>
2021-08-11 16:03:00 +0900

    [add] multiloop02.phpファイル追加
    multiloop02.phpのファイルを追加。
    内容がloop01の内容がloop02に変更され、
    loop01に自作データのテーブル出力機能を作成した。

--- Desktop/sato/05/multiloop01.php
+++ Desktop/sato/05/multiloop01.php
@@ -3,49 +3,33 @@
   <head>
     <meta charset='utf-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <title>連想配列</title>
+    <title>multiloop01</title>
   </head>
   <body>
-      <h1>連想配列</h1>
+      <h1>都道府県</h1>
       <?php
-          $player01 = array(
-              "id" => "3",
-              "name" => "梶谷隆幸",
-              "position" => "外野手",
-              "from" => "島根",
-              "year" => "2007",
-          );
-          $player02 =  array(
-              "id" => "44",
-              "name" => "佐野恵太",
-              "position" => "外野手",
-              "from" => "岡山",
-              "year" => "2017",
-          );
-          $player03 =  array(
-              "id" => "15",
-              "name" => "井納翔一",
-              "position" => "投手",
-              "from" => "東京",
-              "year" => "2013",
-          );
+          $hokkaido = array("北海道");
+          $tohoku  = array("青森", "秋田", "岩手", "山形", "宮城","福島");
+          $kanto = array("神奈川","栃木","群馬","埼玉","千葉","東京","茨城");
+          $tyubu  = array("新潟", "富山", "石川", "福井", "山梨","長野","岐阜","静岡","愛知");
+          $kansai = array("和歌山", "滋賀", "京都", "大阪", "兵庫","奈良","三重");
+          $tyugoku = array("鳥取", "島根", "岡山", "広島", "山口");
+          $sikoku = array("徳島","香川","愛媛","高知");
+          $kyusyu = array("鹿児島","佐賀","長崎","熊本","大分","宮崎","福岡","沖縄");
 
-          $players = array($player01, $player02, $player03);
-          echo "<table><tr>
-                    <th>背番号</th>
-                    <th>名前</th>
-                    <th>ポジション</th>
-                    <th>出身地</th>
-                    <th>入団年</th></tr>";
-          foreach($players as $each){
-              echo  "<tr><td>". $each['id'] . "</td><td>"
-                     . $each['name'] . "</td><td>"
-                     . $each['position'] . "</td><td>"
-                     . $each['from'] . "</td><td>"
-                     . $each['year'] . "</td></tr>";
+          $japan = array($hokkaido, $tohoku, $kanto, $tyubu, $kansai, $tyugoku, $sikoku, $kyusyu);
+          $area = array("北海道","東北","関東","中部","関西","中国","四国","九州");
+          $i=0;
+          echo "<table><tr><th>地方</th><th>県</th></tr>";
+          foreach($japan as $each){
+                  echo "<tr><th>". $area[$i] ."</th>";
+                  foreach($each as $pre){
+                      echo "<td>" . $pre . " </td>";
+                  }
+              echo  "</tr>";
+              $i++;
           }
           echo "</table>";
-    ?>
-
+          ?>
   </body>
 </html>
