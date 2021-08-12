f59d28a71d9c9b4dbba8225119d52433491abc4b
SatoIshii <ishii-sato@webrage.jp>
2021-08-12 17:35:36 +0900

    testtesttest

---
+++ Desktop/C_meibo_system/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <title>index.php</title>
+  </head>
+  <body>
+      <h1>index.php</h1>
+  </body>
+</html>
