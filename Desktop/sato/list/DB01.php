f8a785290de52c0f7cf0304e88ad0de77c658901
SatoIshii <ishii-sato@webrage.jp>
2021-08-12 16:59:47 +0900

    [update] フォルダ名変更
    meibo-> list

---
+++ Desktop/sato/list/DB01.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <title>DB通信テスト</title>
+  </head>
+  <body>
+      <h1>DB通信テスト</h1>
+      <?php
+        $DB_DSN = "mysql:host=localhost; dbname=sishii; charset=utf8";
+        $DB_USER = "webaccess";
+        $DB_PW = "toMeu4rH";
+        $pdo = new PDO($DB_DSN, $DB_USER, $DB_PW);
+
+
+        #SQL文を変数に代入
+        $query_str = "SELECT * FROM test WHERE 1"; // 実行するSQL文を作成して変数に保持
+
+        echo $query_str;                                    // 実行するSQL文を画面に表示するだけ（デバッグプリント
+        $sql = $pdo->prepare($query_str);                   // PDOオブジェクトにSQLを渡す
+        $sql->execute();                                    // SQLを実行する
+        $result = $sql->fetchAll();                         // 実行結果を取得して$resultに代入する
+        echo "<br>";
+
+        #出力
+        foreach($result as $each){
+            echo $each['dishname'] . ":";
+            echo $each['price'] . "円<hr>";
+        }
+      ?>
+  </body>
+</html>
